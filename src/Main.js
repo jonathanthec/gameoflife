8305900b5029138ac52868979b30f9d5dda1ed4d
JonathanChen <jonny29009@yahoo.com>
2020-05-27 14:53:30 -0700

    get components displaying

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import Grid from './Grid';
 
 export default function Main() {
     const [speed, setSpeed] = useState(100);
-    const [rows, setRows] = useState(30);
-    const [cols, setCols] = useState(50);
+    const [rows] = useState(30);
+    const [cols] = useState(50);
     const [generation, setGeneration] = useState(0);
     const [gridFull, setGridFull] = useState(Array(rows).fill().map(() => Array(cols).fill(false)))
+    let intervalId = useRef(null);
 
     const selectBox = (row, col) => {
         let gridCopy = arrayClone(gridFull);
@@ -15,14 +16,80 @@ export default function Main() {
         setGridFull(gridCopy);
     }
 
+    const seed = () => {
+        let gridCopy = arrayClone(gridFull);
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        setGridFull(gridCopy);
+    }
+
+    const start = () => {
+        clearInterval(intervalId.current);
+        intervalId.current = setInterval(play, speed);
+    }
+
+    const pause = () => {
+        clearInterval(intervalId.current);
+    }
+
+    const slow = () => {
+        setSpeed(1000);
+        start();
+    }
+
+    const fast = () => {
+        setSpeed(100);
+        start();
+    }
+
+    const clear = () => {
+        let grid = Array(rows).fill().map(() => Array(cols).fill(false));
+        setGridFull(grid);
+        setGeneration(0);
+    }
+
+    const play = () => {
+        let g = gridFull;
+        let g2 = arrayClone(gridFull);
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                let count = 0;
+                if (i > 0) if (g[i - 1][j]) count++;
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+                if (i > 0 && j < cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < cols - 1) if (g[i][j + 1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < rows - 1) if (g[i + 1][j]) count++;
+                if (i < rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < rows - 1 && j < cols - 1) if (g[i + 1][j + 1]) count++;
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        setGridFull(g2);
+        setGeneration(generation + 1);
+    }
+
+    useEffect(() => {
+        seed();
+    }, [])
+
     return (
         <div>
             <h1>Game of Life</h1>
             <h2>Current Generation: {generation}</h2>
             <ButtonsContainer>
-                <button>Start</button>
-                <button>Pause</button>
-                <button>Clear</button>
+                <button onClick={start}>Start</button>
+                <button onClick={pause}>Pause</button>
+                <button onClick={clear}>Clear</button>
+                <button onClick={slow}>Slow Start</button>
+                <button onClick={fast}>Fast Start</button>
+                <button onClick={seed}>Let's Go Random!</button>
             </ButtonsContainer>
             <Grid
                 rows={rows}
