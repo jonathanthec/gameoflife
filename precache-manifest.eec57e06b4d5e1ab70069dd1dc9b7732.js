313bc2ade657c130e604e147494fdb7034039ec0
JonathanChen <jonny29009@yahoo.com>
2020-05-29 02:59:58 -0700

    Updates

---
+++ precache-manifest.eec57e06b4d5e1ab70069dd1dc9b7732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb453802a6134de3f210b011afefc72",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "a232db639f874b3e3ded",
+    "url": "/gameoflife/static/css/main.ac1b17d6.chunk.css"
+  },
+  {
+    "revision": "4a3e459feaf24df0c312",
+    "url": "/gameoflife/static/js/2.0b9a5285.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gameoflife/static/js/2.0b9a5285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a232db639f874b3e3ded",
+    "url": "/gameoflife/static/js/main.3d67d696.chunk.js"
+  },
+  {
+    "revision": "4b4a0dd4ec2c023c572d",
+    "url": "/gameoflife/static/js/runtime-main.f220b557.js"
+  }
+]);
